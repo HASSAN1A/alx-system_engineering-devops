3a3622c1174fb2df2eb2ca1b80f081dda22135c0
HASSAN1A <okothhassanjuma@gmail.com>
2022-01-18 21:53:43 +0300

    add

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -32,7 +32,7 @@ typedef struct listint_s
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
- * for Holberton project
+ * for the project
  */
 typedef struct skiplist_s
 {
